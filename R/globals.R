f8dddc0eb1d6387ca77313741a3c3e68a7640801
Thomas Petzoldt <thomas.petzoldt@tu-dresden.de>
2026-07-24 11:15:59 +0200

    pass check

---
+++ R/globals.R
@@ -0,0 +1,11 @@
+# Quiet R CMD check NOTES regarding dplyr/NSE global variables
+utils::globalVariables(c(
+  "Sub_Collection",
+  "Authors",
+  "Year",
+  "Title",
+  "DOI",
+  "APA_Citation",
+  "Abstract",
+  "extra"
+))
